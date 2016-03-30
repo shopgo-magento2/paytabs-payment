d75d3d448e02248f99861aef429eefdf5d261a34
Emad Jag <Emad Jag>
2016-03-30 22:12:39 +0200

    add payment config class provider

---
+++ ShopGo/Paytabs/Model/paytabssConfigProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ShopGo\Paytabs\Model;
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+
+class paytabssConfigProvider implements ConfigProviderInterface
+{
+    protected $method;
+
+    public function __construct(
+        Magento\Payment\Helper\Data $Helper
+    ) {
+        $this->method = $Helper->getMethodInstance(ShopGo\Paytabs\Model\paytabs::CODE);
+    }
+
+    public function getConfig()
+    {
+        return $this->method->isAvailable() ? [
+            'payment' => [
+                'paytabs' => [
+                    'redirectUrl' => $this->getRedirectUrl()
+                ]
+            ]
+        ] : [];
+    }
+
+    protected function getRedirectUrl()
+    {
+        return $this->method->getRedirectUrl();
+    }
+}
