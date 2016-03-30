34e89e21aa04a730b38b59ec1f31d8c1eced7cf9
Emad Jag <Emad Jag>
2016-03-30 20:34:48 +0200

    add check account button block

---
+++ ShopGo/Paytabs/Block/System/Config/Button/CheckAccountButton.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ShopGo\Paytabs\Block\System\Config\Button;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Shopgo\Paytabs\Helper\Data;
+
+class CheckAccountButton extends \Magento\Config\Block\System\Config\Form\Field
+{
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        Data $helper,
+        array $data = []
+    ) {
+        $this->_helper = $helper;
+        parent::__construct($context, $data);
+        $this->setTemplate('system/config/check_account_button.phtml');
+    }
+
+
+    public function getButtonHtml()
+    {
+        $button = $this->getLayout()->createBlock(
+            'Magento\Backend\Block\Widget\Button'
+        )->setData(
+            [
+                'id' => 'paytabs_account_checker',
+                'label' => __('Account verification'),
+                'onclick' => 'javascript:checkPaytabsAccount(); return false;',
+            ]
+        );
+
+        return $button->toHtml();
+    }
+
+
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        // Remove scope label
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+        return parent::render($element);
+    }
+
+
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        return $this->_toHtml();
+    }
+
+
+    public function getAjaxCheckUrl()
+    {
+        $response=$this->_helper->PT_validatesecretkey();
+        if(isset($response) && $response['result']=="valid" && isset($response['response_code']) && $response['response_code']=="4000") {
+            return  "Credentials Verified";;
+        }
+        else
+            return  $response['result'];
+    }
+}
