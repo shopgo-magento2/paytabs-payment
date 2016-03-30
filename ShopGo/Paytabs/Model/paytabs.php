44fffa489b5fb6026732acc26f2d8ae3a7056274
Emad Jag <Emad Jag>
2016-03-30 16:51:09 +0200

    add payment model class

---
+++ ShopGo/Paytabs/Model/paytabs.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ShopGo\Paytabs\Model;
+
+class paytabs extends \Magento\Payment\Model\Method\AbstractMethod
+{
+    const CODE = 'paytabs';
+
+    protected $_code = self::CODE;
+
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+        \Magento\Payment\Helper\Data $paymentData,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Payment\Model\Method\Logger $logger
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $paymentData,
+            $scopeConfig,
+            $logger
+        );
+    }
+}
